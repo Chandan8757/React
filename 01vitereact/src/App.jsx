c212c5f5518a43abdbadb585188c45d3b99a22cd
Chandan Kumar <ck846747@gmail.com>
2025-02-23 08:14:45 +0530

    interview

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,43 @@
-import Chai from "./chai"
-function App() {
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+import App from './App.jsx'
 
-  return (
-    <Chai/>
-  )
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
 }
 
-export default App
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank' },
+    'click me to visit google',
+    anotherElement
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    reactElement
+  
+)
+
